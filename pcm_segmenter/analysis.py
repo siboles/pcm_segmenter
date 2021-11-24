f9cf0962507484ecf94d567bf41cd67f04cfbb0e
siboles <scott.sibole@gmail.com>
2021-11-24 13:20:09 -0700

    Finished thickness calculations in analysis.

--- pcm_segmenter/analysis.py
+++ pcm_segmenter/analysis.py
@@ -2,16 +2,17 @@ from typing import List, Tuple
 
 import numpy as np
 import vtk
+from vtk.util import numpy_support
 
 
-def _label_cell_contours(cell_contour: vtk.vtkPolyData):
+def _label_cell_contours(cell_contour: vtk.vtkPolyData) -> vtk.vtkPolyDataConnectivityFilter:
     connected_components = vtk.vtkPolyDataConnectivityFilter()
     connected_components.SetExtractionModeToAllRegions()
     connected_components.ScalarConnectivityOff()
     connected_components.ColorRegionsOn()
     connected_components.SetInputData(cell_contour)
     connected_components.Update()
-    return connected_components.GetOutput()
+    return connected_components
 
 
 def _get_normals(polydata: vtk.vtkPolyData, flip: bool = False) -> vtk.vtkPolyData:
@@ -23,7 +24,7 @@ def _get_normals(polydata: vtk.vtkPolyData, flip: bool = False) -> vtk.vtkPolyDa
     return normals.GetOutput()
 
 
-def _extrude_contour(c: vtk.vtkPolyData, length: float) -> vtk.vtkPolyData:
+def _extrude_contour(c: vtk.vtkPolyData, length: float, flip: bool = False) -> vtk.vtkPolyData:
     extrusion = vtk.vtkLinearExtrusionFilter()
     extrusion.SetExtrusionTypeToNormalExtrusion()
     extrusion.CappingOff()
@@ -38,11 +39,11 @@ def _extrude_contour(c: vtk.vtkPolyData, length: float) -> vtk.vtkPolyData:
     triangle.PassLinesOff()
     triangle.Update()
 
-    extrusion = _get_normals(triangle.GetOutput())
+    extrusion = _get_normals(triangle.GetOutput(), flip=flip)
     return extrusion
 
 
-def _convex_hull_2d(c: vtk.vtkPolyData, spacing: List[float]) -> vtk.vtkPolyData:
+def _convex_hull_2d(c: vtk.vtkPolyData, spacing: float) -> vtk.vtkPolyData:
     convex_hull = vtk.vtkConvexHull2D()
     convex_hull.SetInputData(c)
     convex_hull.OutlineOn()
@@ -57,7 +58,7 @@ def _convex_hull_2d(c: vtk.vtkPolyData, spacing: List[float]) -> vtk.vtkPolyData
     return spline.GetOutput()
 
 
-def _create_extrusion_lists(isocontours: vtk.vtkPolyData,
+def _create_extrusion_lists(isocontours: vtk.vtkPolyDataConnectivityFilter,
                             spacing: List[float]) -> Tuple[List[vtk.vtkPolyData], List[vtk.vtkPolyData]]:
     """For each unique isocontour id create an extrusion of the original contour and its convex hull."""
     convex_hull_spline_spacing = np.min(spacing) / 2.0
@@ -65,14 +66,14 @@ def _create_extrusion_lists(isocontours: vtk.vtkPolyData,
     cell_extrusions = []
     cell_convex_hulls = []
     isolate_cell_contour = vtk.vtkThreshold()
-    isolate_cell_contour.SetInputData(isocontours)
-    for contour_id in range(cell_contours.GetNumberOfExtractedRegions()):
+    isolate_cell_contour.SetInputData(isocontours.GetOutput())
+    for contour_id in range(isocontours.GetNumberOfExtractedRegions()):
         isolate_cell_contour.ThresholdBetween(contour_id, contour_id + 0.5)
         geo_filter = vtk.vtkGeometryFilter()
         geo_filter.SetInputConnection(isolate_cell_contour.GetOutputPort())
         geo_filter.Update()
-        cell_extrusions.append(_extrude_contour(geo_filter.GetOutput(), extrusion_length))
-        cell_convex_hulls.append(_extrude_contour(_convex_hull_2d(cell_isocontour, convex_hull_spline_spacing),
+        cell_extrusions.append(_extrude_contour(geo_filter.GetOutput(), extrusion_length, flip=True))
+        cell_convex_hulls.append(_extrude_contour(_convex_hull_2d(geo_filter.GetOutput(), convex_hull_spline_spacing),
                                                   extrusion_length))
     return cell_extrusions, cell_convex_hulls
 
@@ -89,19 +90,11 @@ def _get_ray_intersection(tree: vtk.vtkOBBTree, origin: List[float],
         return intersection, np.linalg.norm(intersection - origin)
 
 
-def _get_distance(polydata_1: vtk.vtkPolyData, polydata_2: vtk.vtkPolyData) -> Tuple[List[float], List[List[float]]]:
-    distance_filter = vtk.vtkImplicitPolyDataDistance()
-    distance_filter.SetInput(polydata_2)
-    distances = []
-    directions = []
-    for i in range(polydata_1.GetNumberOfPoints()):
-        point = [0.0, 0.0, 0.0]
-        gradient = [0.0, 0.0, 0.0]
-        p1.GetPoint(i, point)
-        distances.append(distance_filter.EvaluateFunction(point))
-        distance_filter.EvaluateGradient(point, gradient)
-        directions.append(gradient)
-    return distance, direction
+def _build_obb_tree(polydata: vtk.vtkPolyData) -> vtk.vtkOBBTree:
+    tree = vtk.vtkOBBTree()
+    tree.SetDataSet(polydata)
+    tree.BuildLocator()
+    return tree
 
 
 def _get_rotation_matrix(surface_angle: float) -> np.ndarray:
@@ -111,8 +104,91 @@ def _get_rotation_matrix(surface_angle: float) -> np.ndarray:
                      [0.0, 0.0, 0.0]], dtype=float)
 
 
+def _initialize_float_array(number_of_tuples: int, number_of_components: int, name: str):
+    array = vtk.vtkFloatArray()
+    array.SetNumberOfComponents(number_of_components)
+    array.SetNumberOfTuples(number_of_tuples)
+    array.SetName(name)
+    return array
+
+
+def _initialize_int_array(number_of_tuples: int, number_of_components: int, name: str):
+    array = vtk.vtkIntArray()
+    array.SetNumberOfComponents(number_of_components)
+    array.SetNumberOfTuples(number_of_tuples)
+    array.SetName(name)
+    return array
+
+
+def _make_thickness_polydata(coordinates: vtk.vtkFloatArray, thicknesses: vtk.vtkFloatArray,
+                             directions: vtk.vtkFloatArray, region_ids: vtk.vtkIntArray) -> vtk.vtkPolyData:
+    points = vtk.vtkPoints()
+    points.SetData(coordinates)
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(points)
+    polydata.GetPointData().AddArray(thicknesses)
+    polydata.GetPointData().AddArray(directions)
+    polydata.GetPointData().AddArray(region_ids)
+    return polydata
+
+
 def calculate_thicknesses(cell_isocontour: vtk.vtkPolyData, ecm_isocontour: vtk.vtkPolyData,
-                          spacing: List[float]) -> np.ndarray:
+                          spacing: List[float], surface_angle: float) -> List[vtk.vtkPolyData]:
     ecm_extrusion = _extrude_contour(ecm_isocontour, np.mean(spacing))
     cell_contours = _label_cell_contours(cell_isocontour)
     cell_extrusions, cell_convex_hulls = _create_extrusion_lists(cell_contours, spacing)
+
+    rotation_matrix = _get_rotation_matrix(surface_angle)
+    region_angle_bounds = np.linspace(0.0, 2.0 * np.pi, num=5, endpoint=True) - np.pi / 4.0
+
+    thickness_polydatas = []
+    for cell_id, (cell, convex_hull) in enumerate(zip(cell_extrusions, cell_convex_hulls)):
+        append_filter = vtk.vtkAppendPolyData()
+        append_filter.AddInputData(ecm_extrusion)
+        for cell_id2, cell2 in enumerate(cell_extrusions):
+            if cell_id == cell_id2:
+                continue
+            else:
+                append_filter.AddInputData(cell2)
+        append_filter.Update()
+        cell_tree = _build_obb_tree(cell)
+        other_tree = _build_obb_tree(append_filter.GetOutput())
+
+        points = numpy_support.vtk_to_numpy(convex_hull.GetPoints().GetData())
+        normals = numpy_support.vtk_to_numpy(convex_hull.GetPointData().GetArray("Normals"))
+        idx = points[:, 2] < 1e-7
+        points = points[idx, :]
+        normals = normals[idx, :]
+
+        number_of_points = points.shape[0]
+        coordinates = _initialize_float_array(number_of_points, 3, "Coordinates")
+        thicknesses = _initialize_float_array(number_of_points, 1, "Thickness")
+        directions = _initialize_float_array(number_of_points, 3, "Direction")
+
+        region_ids = _initialize_int_array(number_of_points, 1, "Region")
+
+        for row in range(number_of_points):
+            # Get point on cell boundary that intersects the local normal of its convex hull
+            intersection_1, thickness_1 = _get_ray_intersection(cell_tree, points[row, :], -normals[row, :], 20.0)
+            # from this intersection point find intersection with PCM boundary along same direction
+            intersection_2, thickness_2 = _get_ray_intersection(other_tree, intersection_1, normals[row, :], 20.0)
+
+            coordinates.SetTuple3(row, *intersection_1)
+            if thickness_2 < 1.0e-7:
+                thicknesses.SetTuple1(row, 0.0)
+            else:
+                thicknesses.SetTuple1(row, thickness_2)
+
+            directions.SetTuple3(row, *normals[row, :])
+
+            rotated_direction = np.dot(rotation_matrix, normals[row, :])
+            relative_to_surface_angle = np.arctan2(rotated_direction[1], rotated_direction[0])
+            if relative_to_surface_angle > region_angle_bounds[-1]:
+                relative_to_surface_angle -= 2.0 * np.pi
+            region = np.digitize([relative_to_surface_angle], region_angle_bounds)[0]
+            region_ids.SetTuple1(row, region)
+
+        thickness_polydatas.append(_make_thickness_polydata(coordinates, thicknesses, directions, region_ids))
+    return thickness_polydatas
+
+
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 import sys
 import pyCellAnalyst as pycell
 sys.path.append("..")
-from pcm_segmenter import config, segment, io
+from pcm_segmenter import config, segment, io, analysis
 
 c = config.parse_config(configuration_file="../configs/test.yaml")
 
@@ -11,8 +11,8 @@ cell = io.read_image_stack(c.cell_image_directories[0], spacing=c.image_spacing[
 ecm_roi = pycell.RegionsOfInterest(ecm, regions_of_interest=c.regions_of_interest[0], start_col=0, slice2d=True)
 cell_roi = pycell.RegionsOfInterest(cell, regions_of_interest=c.regions_of_interest[0], start_col=0, slice2d=True)
 
-ecm_smooth = segment.process_ecm(ecm_roi.images[0], config=c)
-cell_smooth = segment.process_cell(cell_roi.images[0], config=c)
+ecm_smooth = segment.process_ecm(ecm_roi.images[0], conf=c)
+cell_smooth = segment.process_cell(cell_roi.images[0], conf=c)
 
 ecm_smooth.writeAsVTK(name="ecm")
 cell_smooth.writeAsVTK(name="cell")
@@ -22,5 +22,7 @@ cell_segmentation = segment.segment_cell(cell_smooth)
 
 io.write_isocontour(ecm_segmentation.isocontour, name=f"ecm")
 
-contours = segment.combine_segmented_isocontours([ecm_segmentation.isocontour, cell_segmentation.isocontour])
-io.write_isocontour(contours, name="test")
+thickness_polydatas = analysis.calculate_thicknesses(cell_segmentation.isocontour, ecm_segmentation.isocontour,
+                                                     c.image_spacing, c.surface_angles[0])
+
+io.write_isocontour(thickness_polydatas[0], name="thickness")
